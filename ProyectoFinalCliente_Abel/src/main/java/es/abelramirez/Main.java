3a0f1192c402b015524aface25705117a5071c69
Abel-RV <ramirezvillarejoabel@gmail.com>
2026-02-13 10:27:58 +0100

    AÑadidos los candidatos

--- ProyectoFinalCliente_Abel/src/main/java/es/abelramirez/Main.java
+++ ProyectoFinalCliente_Abel/src/main/java/es/abelramirez/Main.java
@@ -1,9 +1,83 @@
 package es.abelramirez;
 
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        
+        System.setProperty("javax.net.ssl.trustStore", "client_truststore.jks");
+        System.setProperty("javax.net.ssl.trustStorePassword", "123456");
+
+        String host = "localhost";
+        int puerto= 1000;
+
+        try{
+            SSLSocketFactory sf =(SSLSocketFactory) SSLSocketFactory.getDefault();
+            SSLSocket socket = (SSLSocket) sf.createSocket(host,puerto);
+            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+            DataInputStream dis = new DataInputStream(socket.getInputStream());
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Conexión establecida.");
+
+            boolean salir = false;
+            while (!salir) {
+                // Menú de opciones
+                System.out.println("\n--- SISTEMA DE VOTACIÓN ---");
+                System.out.println("1. Listar Candidatos");
+                System.out.println("2. Votar");
+                System.out.println("3. Ver Resultados");
+                System.out.println("4. Salir");
+                System.out.print("Elige una opción: ");
+
+                String opcion = sc.nextLine();
+                String comando = "";
+
+                // Mapeo de opciones a comandos del protocolo
+                switch (opcion) {
+                    case "1": comando = "LISTAR"; break;
+                    case "2": comando = "VOTAR"; break;
+                    case "3": comando = "RESULTADOS"; break;
+                    case "4": comando = "END"; break;
+                    default:
+                        System.out.println("Opción no válida.");
+                        continue;
+                }
+
+                // Enviar comando
+                dos.writeUTF(comando);
+
+                // Manejar respuestas según el protocolo
+                if (comando.equals("END")) {
+                    salir = true;
+                } else if (comando.equals("VOTAR")) {
+                    // El caso VOTAR tiene un diálogo extra
+                    System.out.println(dis.readUTF()); // Servidor: "Introduce el nombre..."
+                    String voto = sc.nextLine();
+                    dos.writeUTF(voto);                // Enviamos el nombre
+                    System.out.println("SERVIDOR: " + dis.readUTF()); // Confirmación
+                } else {
+                    // LISTAR y RESULTADOS devuelven una lista directa (con corchetes, como querías)
+                    System.out.println("RESPUESTA SERVIDOR:");
+                    System.out.println(dis.readUTF());
+                }
+            }
+            socket.close();
+            System.out.println("Desconectado.");
+
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }finally {
+
+        }
     }
 }
--- ProyectoFinalServidor_Abel/src/main/java/es/abelramirez/Main.java
+++ ProyectoFinalServidor_Abel/src/main/java/es/abelramirez/Main.java
@@ -1,9 +1,30 @@
 package es.abelramirez;
 
+import es.abelramirez.threads.HiloServidor;
+
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.setProperty("javax.net.ssl.keyStore", "ssl/server_keystore.jks");
+        System.setProperty("javax.net.ssl.keyStorePassword", "123456");
 
+        ModeloCandidatos mc = new ModeloCandidatos();
+        ExecutorService pool = Executors.newFixedThreadPool(5);
+        int puerto =1000;
+        try{
+            SSLServerSocketFactory ssf = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+            SSLServerSocket serverSocket = (SSLServerSocket) ssf.createServerSocket();
+            HiloServidor hiloServidor = new HiloServidor(serverSocket,pool,mc);
+            hiloServidor.start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
